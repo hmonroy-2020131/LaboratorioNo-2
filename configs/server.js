b9a65c5c75120bacfa58b0c016689820122da175
hmonroy-2020131 <hmonroy-2020131@kinal.edu.gt>
2025-02-11 18:49:11 -0600

    Conexion a la base de datos terminada

---
+++ configs/server.js
@@ -0,0 +1,45 @@
+'use strict';
+ 
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import { dbConnection } from './mongo.js';
+import limiter from '../src/middlewares/validar-cant-peticion.js';
+ 
+const middlewares = (app) => {
+    app.use(express.urlencoded({ extended: false }));
+    app.use(cors());
+    app.use(express.json());
+    app.use(helmet());
+    app.use(morgan('dev'));
+    app.use(limiter);
+}
+ 
+ 
+const conectarDB = async () => {
+    try{
+        await dbConnection();
+        console.log("Conexion a la base de datos exitosa");
+    }catch(error){
+        console.error('Error Conectando a la base de datos', error);
+        process.exit(1);
+    }  
+}
+ 
+export const initServer = async () =>{
+    const app = express();
+    const port = process.env.PORT || 3001;
+
+    try {
+        middlewares(app);
+        conectarDB();
+        routes(app);
+        app.listen(port);
+        console.log(`Server running on port: ${port}`);
+    } catch (err) {
+        console.log(`Server init failed: ${err}`)
+    }
+ 
+    
+}
